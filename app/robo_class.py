a6dab15039fc004decbef2f9e03ae3b876612762
rohanmalhotra4622 <44009676+rohanmalhotra4622@users.noreply.github.com>
2019-06-16 18:53:42 -0400

    Get Latest Refresh Date

--- app/robo_class.py
+++ app/robo_class.py
@@ -13,8 +13,10 @@ response = requests.get(request_url)
 
 # parses the response.test which is string into a dictionary
 parsed_response = json.loads(response.text)
+last_refreshed = parsed_response['Meta Data']['3. Last Refreshed']
 
-breakpoint()
+
+#breakpoint()
 
 #quit()
 
@@ -27,7 +29,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
