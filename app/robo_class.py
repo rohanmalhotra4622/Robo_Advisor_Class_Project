cc7617fbeb702f5c9a83071ebe39bb0aa13d3786
rohanmalhotra4622 <44009676+rohanmalhotra4622@users.noreply.github.com>
2019-06-18 20:31:53 -0400

    Write example data to csv

--- app/robo_class.py
+++ app/robo_class.py
@@ -2,6 +2,8 @@
 
 import requests
 import json
+import csv
+import os
 
 def to_usd(my_price):
     return "${0:.2f}".format(my_price)
@@ -45,6 +47,23 @@ recent_low = min(low_prices)
 # INFO Output
 
 
+#csv_file_path = 'data/prices.csv'
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
+
+
+
+
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -59,5 +78,10 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
+print(f"WRITING DATA TO CSV: {csv_file_path}...")
+print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+
+
